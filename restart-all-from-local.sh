43276593421a04ca877709e0c8c66615bd6ac00c
Noda Shimpei <noda.sin@gmail.com>
2017-11-14 16:28:11 +0900

    Add exec permittion for all user

--- restart-all-from-local.sh
+++ restart-all-from-local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-ssh -A isucon@ec2-13-114-142-226.ap-northeast-1.compute.amazonaws.com webapp/restart.sh
+ssh -A isucon@ec2-13-114-142-226.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart.sh
