7b20e38cbdca0fe8fb5c2a0c613912f979655feb
Noda Shimpei <noda.sin@gmail.com>
2017-11-14 23:32:34 +0900

    Add service files

--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 set -e
 cd /home/isucon/webapp
 git pull
-docker-compose build
-docker-compose up -d
+(cd nodejs && npm install && npm cache clean)
+(cd react && npm install && npm cache clean && NODE_ENV=production npm run build)
