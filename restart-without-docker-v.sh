ab5fe1c865fb2221ae6b42f14ad231bdcc4570ac
Noda Shimpei <noda.sin@gmail.com>
2017-11-15 13:06:53 +0900

    Add nginx.conf

--- restart-without-docker-v.sh
+++ restart-without-docker-v.sh
@@ -4,6 +4,8 @@ cd /home/isucon/webapp
 git pull
 (cd nodejs && npm install && npm cache clean)
 (cd react && npm install && npm cache clean && NODE_ENV=production npm run build)
+(\cp etc/nginx/nginx.conf /etc/nginx/nginx.conf)
 sudo systemctl restart mysql
 sudo systemctl restart nodejs.service
 sudo systemctl restart react.service
+sudo systemctl restart nginx.service
