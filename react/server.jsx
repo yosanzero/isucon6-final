d1b35231f7e143b8201675c219cea018e9917ed4
Noda Shimpei <noda.sin@gmail.com>
2017-11-16 16:25:08 +0900

    Change server port and protocol https -> http

--- react/server.jsx
+++ react/server.jsx
@@ -1,6 +1,6 @@
 import newrelic from 'newrelic';
 import express from 'express';
-import https from 'https';
+import http from 'http';
 import fs from 'fs';
 import path from 'path';
 import React from 'react';
@@ -21,17 +21,6 @@ const apiBaseUrl = process.env.API;
 if (!apiBaseUrl) {
   throw 'Please set environment variable API=http://...';
 }
-if (!process.env.SSL_KEY) {
-  throw 'Please set environment variable SSL_KEY=/path/to/server.key';
-}
-if (!process.env.SSL_CERT) {
-  throw 'Please set environment variable SSL_CERT=/path/to/server.crt';
-}
-
-const options = {
-  key: fs.readFileSync(process.env.SSL_KEY),
-  cert: fs.readFileSync(process.env.SSL_CERT),
-};
 
 const app = express();
 
@@ -105,8 +94,8 @@ app.get('*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 443;
-https.createServer(options, app).listen(PORT);
+const PORT = process.env.PORT || 8080;
+http.createServer(app).listen(PORT);
 
 function createHtml(appHtml, scriptTag, csrfToken) {
   return `<!DOCTYPE html>
