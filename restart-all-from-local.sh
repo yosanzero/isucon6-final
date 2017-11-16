cc7d550e499b4b2dcc2ab52abdab6a53bfbaf7a3
Noda Shimpei <noda.sin@gmail.com>
2017-11-16 16:17:21 +0900

    Ignore docker version from deploy targets

--- restart-all-from-local.sh
+++ restart-all-from-local.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -xe
-ssh -A isucon@ec2-13-114-142-226.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-docker-v.sh
 ssh -A isucon@ec2-54-250-246-146.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-without-docker-v.sh
