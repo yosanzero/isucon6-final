b3e44567602c40a704712eb4c436fd5bf31d1b18
Noda Shimpei <noda.sin@gmail.com>
2017-11-16 16:28:32 +0900

    Fixed restart script

--- restart-without-docker-v.sh
+++ restart-without-docker-v.sh
@@ -4,7 +4,7 @@ cd /home/isucon/webapp
 git pull
 (cd nodejs && npm install && npm cache clean)
 (cd react && npm install && npm cache clean && NODE_ENV=production npm run build)
-(\cp etc/nginx/nginx.conf /etc/nginx/nginx.conf)
+(sudo cp etc/nginx/nginx.conf /etc/nginx/nginx.conf)
 sudo systemctl restart mysql
 sudo systemctl restart nodejs.service
 sudo systemctl restart react.service
