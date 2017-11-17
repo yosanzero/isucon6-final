c46f37561b9fbd2c642a863fed755312c7adce09
Noda Shimpei <noda.sin@gmail.com>
2017-11-17 22:24:49 +0900

    Revert "Append init script of stroke_count"
    This reverts commit 17517506272e400bb1a05f03b64f31dfd2e84211.

--- sql/03_add_stroke_count_column.sql
+++ sql/03_add_stroke_count_column.sql
@@ -1,3 +1,2 @@
 ALTER TABLE rooms ADD stroke_count bigint(20) NOT NULL DEFAULT 0 AFTER name;
 ALTER TABLE rooms ADD INDEX count_idx(stroke_count);
-UPDATE rooms AS r SET r.stroke_count = (SELECT count(*) FROM strokes WHERE room_id = r.id);
