9f614120e6f50a71b4c9a70ec44ab7de9894bb48
Noda Shimpei <noda.sin@gmail.com>
2017-11-22 15:45:14 +0900

    Update mysql host

--- restart-without-docker-v.sh
+++ restart-without-docker-v.sh
@@ -5,7 +5,7 @@ git fetch origin
 git reset --hard origin/master
 sudo chmod -R 777 /home/isucon/webapp/react/public/
 (cd nodejs && npm install && npm cache clean)
-(cd nodejs && node init.js)
+(cd nodejs && MYSQL_HOST=172.29.2.63 node init.js)
 (cd react && npm install && npm cache clean && NODE_ENV=production npm run build)
 sudo cp etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
