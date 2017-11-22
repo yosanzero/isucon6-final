b8f55dc1a28c2f3344a4005fdb307e3aac30457d
Noda Shimpei <noda.sin@gmail.com>
2017-11-22 11:04:08 +0900

    Add server

--- restart-all-from-local.sh
+++ restart-all-from-local.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -xe
 ssh -A isucon@ec2-54-250-246-146.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-without-docker-v.sh
+ssh -A isucon@ec2-54-238-184-241.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-without-docker-v.sh
