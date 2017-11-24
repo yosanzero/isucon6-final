976c28f887aa567c6191cad4f2f975950049ddd6
noda <noda.sin@gmail.com>
2017-11-24 03:13:29 +0000

    Add config

--- restart-all-from-local.sh
+++ restart-all-from-local.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -xe
+scp restart-without-docker-v.sh isucon@ec2-54-250-246-146.ap-northeast-1.compute.amazonaws.com:webapp/restart-without-docker-v.sh
+scp restart-without-docker-v.sh isucon@ec2-54-238-184-241.ap-northeast-1.compute.amazonaws.com:webapp/restart-without-docker-v.sh
+scp restart-without-docker-v.sh isucon@ec2-54-249-24-93.ap-northeast-1.compute.amazonaws.com:webapp/restart-without-docker-v.sh
 ssh -A isucon@ec2-54-250-246-146.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-without-docker-v.sh
 ssh -A isucon@ec2-54-238-184-241.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-without-docker-v.sh
+ssh -A isucon@ec2-54-249-24-93.ap-northeast-1.compute.amazonaws.com /bin/bash webapp/restart-without-docker-v.sh
--- restart-without-docker-v.sh
+++ restart-without-docker-v.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -e
 cd /home/isucon/webapp
+
 git fetch origin
 git reset --hard origin/master
+
 sudo chmod -R 777 /home/isucon/webapp/react/public/
 (cd nodejs && npm install && npm cache clean)
 (cd nodejs && MYSQL_HOST=172.29.2.63 node init.js)
 (cd react && npm install && npm cache clean && NODE_ENV=production npm run build)
+
 sudo cp etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
+sudo cp etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo cp etc/redis/redis.conf /etc/redis/redis.conf
 sudo cp nodejs/nodejs.service /etc/systemd/system/
 sudo cp react/react.service /etc/systemd/system/
+
 sudo systemctl daemon-reload
-sudo systemctl restart mysql
 sudo systemctl restart nodejs
+sudo systemctl restart mysql
+sudo systemctl restart redis-server
 sudo systemctl restart react
 sudo systemctl restart nginx
-sudo systemctl restart redis-server
+
 echo "" | sudo tee /var/log/mysql/slow.log
 echo "" | sudo tee /var/log/nginx/access.log
